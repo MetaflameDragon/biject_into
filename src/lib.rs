2d8857d824f210a0bb6e7067a59dcc872858a8b0
Metaflame <metaflamedragon@gmail.com>
2025-06-17 14:10:48 +0200

    something that works?

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,76 @@
+#![feature(trace_macros)]
+
 macro_rules! bijection {
     ($first_ty:ty, $second_ty:ty,
-        $($left:tt)*
+        {$($bij:tt)*}
+    ) => {
+        bijection!(@
+            ($first_ty, $second_ty)
+            {}
+            {}
+            ($($bij)*)
+            ($($bij)*)
+        )
+    };
+
+    (@
+    ($first_ty:ty, $second_ty:ty)
+        { $($first_done:tt)* }
+        { $($second_done:tt)* }
+        ($first_pat:pat_param => $first_expr:expr      , $($first_rest:tt )*)
+        ($second_expr:expr    => $second_pat:pat_param , $($second_rest:tt)*)
+    ) => {
+        bijection!(@
+            ($first_ty, $second_ty)
+            {
+                $($first_done)*
+                $first_pat => $first_expr,
+            }
+            {
+                $($second_done)*
+                $second_pat => $second_expr,
+            }
+            ($($first_rest)*)
+            ($($second_rest)*)
+        )
+    };
+
+    (@
+    ($first_ty:ty, $second_ty:ty)
+        { $($first_done:tt)* }
+        { $($second_done:tt)* }
+        ($first_pat:pat_param => $first_expr:expr      $(,)?)
+        ($second_expr:expr    => $second_pat:pat_param $(,)?)
+    ) => {
+        bijection!(@
+            ($first_ty, $second_ty)
+            {
+                $($first_done)*
+                $first_pat => $first_expr,
+            }
+            {
+                $($second_done)*
+                $second_pat => $second_expr,
+            }
+            ()
+            ()
+        )
+    };
+
+
+    (@
+    ($first_ty:ty, $second_ty:ty)
+        { $($first_done:tt)* }
+        { $($second_done:tt)* }
+        ()
+        ()
     ) => {
         // Extra block so this can be used without a semicolon (expands to an expression)
         {
             impl From<$first_ty> for $second_ty {
                 fn from(value: $first_ty) -> Self {
                     match value {
-                        // $($left => { todo!() },)+
-                        _ => todo!(),
+                        $($first_done)*
                     }
                 }
             }
@@ -16,8 +78,7 @@ macro_rules! bijection {
             impl From<$second_ty> for $first_ty {
                 fn from(value: $second_ty) -> Self {
                     match value {
-                        //$($right => { todo!() },)+
-                        _ => todo!(),
+                        $($second_done)*
                     }
                 }
             }
@@ -42,9 +103,11 @@ mod tests {
             Y,
         }
 
+        trace_macros!(true);
         bijection!(Foo, Bar, {
-            Foo::A = Bar::X,
-            Foo::B = Bar::Y
-        })
+            Foo::A => Bar::X,
+            Foo::B => Bar::Y,
+        });
+        trace_macros!(false);
     }
 }
